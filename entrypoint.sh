5229556be3d86aa9ac3767c6e3316c3f2c6f5e48
Arvind Rao <arvind.rao@here.com>
2020-04-27 08:41:58 +0200

    check all cpp and h files under github workspace.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
-###############################################################################
-#                                entrypoint.sh                                #
-###############################################################################
-# Checks all C files (.c and .h) in the GitHub workspace for conforming to
-# clang-format. If any C files are incorrectly formatted, the script lists them
-# and exits with 1.
-# Define your own formatting rules in a .clang-format file at your repository
-# root. Otherwise, the LLVM style guide is used as a default.
-
-
-###############################################################################
-#                             format_diff function                            #
-###############################################################################
 # Accepts a filepath argument. The filepath passed to this function must point
-# to a .c or .h file. The file is formatted with clang-format and that output is
+# to a .cpp or .h file. The file is formatted with clang-format-9 and that output is
 # compared to the original file.
 format_diff(){
     local filepath="$1"
@@ -33,7 +20,7 @@ cd "$GITHUB_WORKSPACE" || exit 1
 # All files improperly formatted will be printed to the output.
 acc=0;
 
-files=$(find ./src ./include ./test -regextype posix-extended  -regex '.*.(h|cpp)')
+files=$(find . -regextype posix-extended  -regex '.*.(h|cpp)')
 for f in $files; do
     format_diff $f
     acc=$((acc + $?))
